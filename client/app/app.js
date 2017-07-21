3986eae41106ff787b4a5dcd0ff5c62850eba4e0
InnovationBham_1 <InnovationBham_1@UABiLab-1s-iMac.local>
2017-07-20 21:07:44 -0500

    final commit

--- client/app/app.js
+++ client/app/app.js
@@ -1,28 +1,70 @@
-var app = angular.module("myApp", ["ngRoute", "myPosts"]);
+var app = angular.module('myApp', ['ngRoute', ])
+
 app.config(function($routeProvider){
     $routeProvider
     .when("/", {
         templateUrl: "../views/home.html",
-        controller: "homecontroller"
     })
     .when("/add", {
-        templateUrl: "../views/add.html"
+        templateUrl: "../views/add.html",
     })
     .when("/list", {
         templateUrl: "../views/list.html"
     })
-    .when("/single", {
-        templateUrl: "../views/single.html"
+    .when("/single/:id", {
+        templateUrl: "../views/single.html",
     })
 });
-var post = angular.module('myPosts', []);
-post.controller('homecontroller', function(){
+
+app.controller('addController', function($scope,$http) {
+    $scope.posts = function(){ 
+        var info = ({
+            user : $scope.user,
+            alias : $scope.alias,
+            message : $scope.message
+        });
+        console.log(info)
+    $http.post('http://localhost:3000/api/chirps', info)
+        .then(function(response){ 
+            alert('Hello Tenno');
+        }, function myError(response){
+            alert('Something went wrong')
+        });
+        
+    }
+});
+
+app.controller('listController',  function($http, $scope, $location){
+    $http.get('http://localhost:3000/api/chirps')
+       .then(function(success){
+        $scope.data=success.data
+    },  function(err){
+        alert('something went wrong')
+    })
+    $scope.getId=function(id){
+        console.log(id)
+        $location.path('/single/' + id)
+    }
+
+    $scope.remove=function(id){
+    $http.delete('http://localhost:3000/api/chirps/' + id)
+        .then(function(success){
+         $location.path('/list/');
+        }
+        )}
     
-});
-post.controller('PostController', function() {
-    this.post = {};
-        this.addpost = function(user){
-            user.post.push(this.post);
-            this.post = {};
-    };
-});
+}); 
+
+app.controller('singleController',  function($http, $scope, $routeParams, $window, $location){
+    var id = $routeParams.id;
+    $http.get('http://localhost:3000/api/chirps/' + id)
+    .then(function(success){
+        $scope.data=success.data
+    });
+    $scope.remove=function(id){
+    $http.delete('http://localhost:3000/api/chirps/' + id)
+        .then(function(success){
+        $location.path('/list/');;
+        }
+        )}
+    })
